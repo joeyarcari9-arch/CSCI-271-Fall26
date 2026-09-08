21e7586c8953dafae1c9ccfb11bbce41b3fe1307
joeyarcari9-arch <joeyarcari9@gmail.com>
2026-09-08 17:47:29 -0400

    add first C++ practice program

---
+++ Week-01/main.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    int age; // this is intger, this will define age
+    cout<<"Hello World\n"; /* this is for printing purpose, for display */
+    cout<<"This is day 2\n";
+    cout<<"Take an user input for age\n";
+    cin>> age; 
+    
+    /*
+    
+    
+    
+    
+    */
+    return 0;
+}
